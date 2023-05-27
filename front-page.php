2ff453d5a0a62e076488ec5731ddae519b60ee63
Alvin KN <alvinkn.dev@gmail.com>
2023-05-27 23:59:24 +0700

    complete slice kementrian page

--- front-page.php
+++ front-page.php
@@ -1,4 +1,19 @@
-<?php 
+<?php
 wp_head();
-    require('homepage.php');
-?>
+require('homepage.php');
+// require('page-menko.pelayanan.php');
+// require('page-menteri.sekkab.php');
+// require('page-menteri.keuangan.php');
+// require('page-menteri.pao.php');
+// require('page-menteri.kominfo.php');
+// require('page-menteri.dagri.php');
+// require('page-menteri.lugri.php');
+// require('page-menteri.rispensi.php');
+// require('page-menteri.bumm.php');
+// require('page-menteri.budpora.php');
+// require('page-menteri.psdm.php');
+// require('page-menteri.advo.php');
+// require('page-menteri.kastrat.php');
+// require('page-menteri.p3.php');
+// require('page-menteri.sosma.php');
+// require('page-menteri.lh.php');
